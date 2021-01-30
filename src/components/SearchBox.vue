4de236dffecbfb17ebb86a2662d3758ee84635f0
norecords <radio@norecords.org>
2021-01-30 07:09:48 +0100

    Push router to load player profile from external link

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <div class="card bg-dark">
+                <div class="card-body">
+                    <h4>Search Players</h4>
+                    <div class="form-group">
+                        <input
+                        class="form-control"
+                        v-model="term"
+                        type="search"
+                        placeholder="Player name"
+                        >
+                    </div>
+                    <button
+                        class="btn btn-outline-light mt-2"
+                        @click.prevent="search"
+                    >
+                        Search
+                    </button>
+                    <ul v-if="players.length" class="list-group mt-3">
+                        <li class="list-unstyled">{{ playerFound }}</li>
+                        <li
+                            v-for="item in players"
+                            :key="item"
+                            v-on:click="loadProfile(item.playerId)"
+                            class="list-group-item list-group-item-action list-group-item-dark urlpointer"
+                        >
+                        <!--
+                        <img
+                            v-bind:src="require(`@/assets/flags/${countryFlag(item.flag.toLowerCase())}.png`)"
+                            id="country"
+                            title="Flags"
+                            width="24px"
+                            height="18px"
+                        />
+                        -->
+                        {{item.lastName}}
+                        </li>
+                    </ul>
+                    <ul
+                      v-else
+                      class="list-group mt-3"
+                    >
+                      <li class="list-unstyled">
+                          {{ playerFound }}
+                      </li>
+                    </ul>
+                </div>
+                <div
+                    class="card-footer pb-1 pt-2"
+                    v-if="players.length"
+                >
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { debounce } from 'lodash'
+export default {
+  data () {
+    return {
+      players: {},
+      playerFound: '',
+      term: '',
+    }
+  },
+    methods: {
+    loadProfile: function (id) {
+      this.$emit('playerid', id)
+      this.$router.push({
+        path: '/player',
+        query: {
+          id: id
+        }
+      })
+    },
+    search: function () {
+    // eslint-disable-next-line no-console
+      console.log(`Checking name: ${this.term}`)
+      this.$api
+        .get('https://cigalestrike.norecords.org/vue/apiv3.php', {
+          params: {
+            q: 'username',
+            name: this.term
+          }
+        })
+        .then(res => {
+          this.players = res.data.user
+          this.playerFound = res.data.user.length >= 1 ? 'Found ' + res.data.user.length + (res.data.user.length === 1 ? ' player' : ' players') : 'No player found!'
+          // console.log(this.players)
+        })
+        .catch(err => {
+        // eslint-disable-next-line no-console
+          console.log(err)
+        })
+    }
+  },
+  created () {
+    this.debounceName = debounce(this.search, 1000)
+  },
+  watch: {
+    term () {
+      if (!this.term) return
+      this.debounceName()
+    }
+  }
+}
+
+</script>
--- src/views/Player.vue
+++ src/views/Player.vue
@@ -2,69 +2,16 @@
   <div>
     <div v-if="!showSearch">
     <button 
-      v-on:click="showSearch = !showSearch, show = !show"
+      v-on:click="searchButton"
       class="btn btn-outline-light"
     >
     Search
     </button>
     </div>
     <transition name="fade">
-    <div v-if="showSearch" class="row">
-        <div class="col-12">
-            <div class="card bg-dark">
-                <div class="card-body">
-                    <h4>Search Players</h4>
-                    <div class="form-group">
-                        <input
-                        class="form-control"
-                        v-model="term"
-                        type="search"
-                        placeholder="Player name"
-                        >
-                    </div>
-                    <button
-                        class="btn btn-outline-light mt-2"
-                        @click.prevent="search"
-                    >
-                        Search
-                    </button>
-                    <ul v-if="players.length" class="list-group mt-3">
-                        <li class="list-unstyled">{{ playerFound }}</li>
-                        <li
-                            v-for="item in players"
-                            :key="item"
-                            v-on:click="loadProfile(item.playerId)"
-                            class="list-group-item list-group-item-action list-group-item-dark urlpointer"
-                        >
-                        <!--
-                        <img
-                            v-bind:src="require(`@/assets/flags/${countryFlag(item.flag.toLowerCase())}.png`)"
-                            id="country"
-                            title="Flags"
-                            width="24px"
-                            height="18px"
-                        />
-                        -->
-                        {{item.lastName}}
-                        </li>
-                    </ul>
-                    <ul
-                      v-else
-                      class="list-group mt-3"
-                    >
-                      <li class="list-unstyled">
-                          {{ playerFound }}
-                      </li>
-                    </ul>
-                </div>
-                <div
-                    class="card-footer pb-1 pt-2"
-                    v-if="players.length"
-                >
-                </div>
-            </div>
-        </div>
-    </div>
+      <div v-if="showSearch">
+        <SearchBox v-on:playerid="loadProfile"/>
+      </div>
     </transition>
     <transition name="fade">
       <div v-if="show">
@@ -75,55 +22,35 @@
 </template>
 
 <script>
+import SearchBox from '../components/SearchBox'
 import PlayerProfile from '../components/PlayerProfile'
-import { debounce } from 'lodash'
 export default {
-  components: { PlayerProfile },
+  components: {
+    SearchBox, 
+    PlayerProfile 
+  },
   data () {
     return {
-      players: [],
-      playerFound: '',
       show: false,
-      showSearch: true,
-      term: '',
+      showSearch: false,
       id: ''
     }
   },
+  mounted () {
+    this.$route.query.id ? this.loadProfile(this.$route.query.id) : this.showSearch = true
+  
+  },
     methods: {
-    loadProfile: function (id) {
-      this.showSearch = false
-      this.id = id
-      console.log(this.id)
-      return this.show = true
+    searchButton () {
+      this.showSearch = !this.showSearch
+      if (this.$route.query.id) this.$router.push({ path: '/player', query: ''})
+      this.show = !this.show  
     },
-    search: function () {
-    // eslint-disable-next-line no-console
-      console.log(`Checking name: ${this.term}`)
-      this.$api
-        .get('https://cigalestrike.norecords.org/vue/apiv3.php', {
-          params: {
-            q: 'username',
-            name: this.term
-          }
-        })
-        .then(res => {
-          this.players = res.data.user
-          this.playerFound = res.data.user.length >= 1 ? 'Found ' + res.data.user.length + (res.data.user.length === 1 ? ' player' : ' players') : 'No player found!'
-          console.log(this.players)
-        })
-        .catch(err => {
-        // eslint-disable-next-line no-console
-          console.log(err)
-        })
-    }
-  },
-  created () {
-    this.debounceName = debounce(this.search, 1000)
-  },
-  watch: {
-    term () {
-      if (!this.term) return
-      this.debounceName()
+    loadProfile (value) {
+      this.id = value
+      this.show = true
+      this.showSearch = false
+      console.log('id: ' + value)
     }
   }
 }
